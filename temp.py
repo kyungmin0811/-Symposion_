eb076eb4ff33a9b5c73f80f311639c246d02321a
Kyungmin <arikim0811@gmail.com>
2024-07-17 15:00:05 +0900

    [A] 문제 분석 후, 정규표현식으로 문항 스크랩 파트 일부 완성 / 보완 필요

--- temp.py
+++ temp.py
@@ -1,8 +1,8 @@
 
 import sys, pathlib, pymupdf
-fname = r"resolutions\2024_s.pdf"
+fname = r"resolutions\2014_s.pdf"
 with pymupdf.open(fname) as doc:  # open document
     text = "".join([page.get_text() for page in doc])
 
-with open("log.txt", 'w') as f:
+with open("log.txt", 'w', encoding='utf-8') as f:
     f.write(text)
--- word_count.py
+++ word_count.py
@@ -1,5 +1,4 @@
-import sys, pathlib, pymupdf
-fname = r"resolutions\2025_6.pdf"
+import pymupdf, re
 error_count = 0
 
 
@@ -10,51 +9,60 @@ def scan_qs(path, critical, nq):
     #path: sat pdf path / critical : ex. "[41~43]___.~" point. / nq : number of questions
 
     print(f"\n\n\n**********{path}**********")
-    with pymupdf.open(fname) as doc:  # open document
+    with pymupdf.open(r"resolutions\\"+path) as doc:  # open document
         ws = "".join([page.get_text() for page in doc]) #whole sentence of document (str)
 
     #line 17-39:scanning index of question
     num = 18 #question No.
     QidxLst = [] #Question Index List
     ContinueQLst = [] #Continueal Question List
+    preQidx = ws.find(f"[{num}～") + len(str(num)) + 5
     while num <= nq:
-        if (ws.find(f"[{num}～") != -1 and ws.find(f"[{num}～") > preQidx and num >= critical):
+        if (ws.find(f"[{num}～") != -1 and ws.find(f"[{num}～") > preQidx):
             start = num
             preQidx = ws.find(f"[{num}～") + len(str(num)) + 5
             end = int(ws[preQidx-3:preQidx-1])
             print(f"{path}) ques.{start}~{end} cont")
-            QidxLst += [ws.find(f"[{num}～") for i in range(end - start + 1)]
-            ContinueQLst += [i for i in range(start, end)]
-            num += end - start 
-            
-        elif(ws.find(f"\n{num}.") != -1 and ws.find(f"[{num}～") > preQidx):
+            QidxLst += [ws.find(f"[{num}～")] + [-5 for i in range(end - start)]
+            ContinueQLst += [num, end-start]
+            num += end - start     
+        elif(ws.find(f"\n{num}.") != -1 and ws.find(f"\n{num}.") > preQidx):
             print(f"{path}) ques.{num} sing")
             QidxLst.append(ws.find(f"\n{num}."))
+            preQidx = ws.find(f"\n{num}.")
         else:
             print(f"{path}) ques.{num} is not found.")
             QidxLst.append(-10)
             error_count += 1
         num += 1
+    QidxLst.append(0)
+    next = 0 #next index in questions
+    titles = [] #title of question
+    sentences = []
+    for n in range(18, num):
+        if (QidxLst[n-18] != -5):
+            if (n in ContinueQLst[::2] and n >= critical):
+                next = ContinueQLst[ContinueQLst[::2].index(n)*2  + 1] + n + 1  #ContinueQLst start from 18, next start from 0 
+                print(f"{n}~{next-1} is continuous questions")
+            else:
+                next = n+1
+
+            print(f"reading {n} (~ prev. {next}).... ", end='')
+            print(f"<idx : {QidxLst[n-18]} ~ {QidxLst[next-18]}-1>....", end='')
+            
+            title = re.compile(str(n) + r"\. .*?(은\?|시오\.)", flags = re.DOTALL).search(ws)
+            print(title.Match, end="")
+            txt = ws[QidxLst[n-18]:QidxLst[next-18]-1]
+            eng_str = re.sub(r"[^a-zA-Z.? ,'xd ]+", "", txt) # remain only Eng
+            sentences.append(re.sub("[.?]", ".\n", eng_str))
+
+            print('done!')
     
-    #extrect sentence from questions
-    num = 18
-    ignore = False # continue situation
-    while num <= nq:
+    with open(f"resolution_txt\{path}.txt", 'w') as f:
+        for s_num, sentence in enumerate(sentences):
+            f.write("\n"*3 + f"**********N.{s_num+18}**********\n")
+            f.write(sentence)
 
-        if (not ignore):
-            index = num-18 
-            if (num)
-            sentence = ws[QidxLst[index] : QidxLst[index+1]-1]
-        
-
-        
-        if (num in ContinueQLst):
-            ignore = True
-        else:
-            ignore = False
-
-        num += 1
-        
 
 for year in range(2014, 2025):
     scan_qs(f"{year}_s.pdf", 41, 45)
